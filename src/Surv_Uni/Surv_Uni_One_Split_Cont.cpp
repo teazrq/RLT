815294335d9cd99078cb41d83211ae366f58c9a1
sarahef2 <sarahef2@illinois.edu>
2021-04-08 11:17:32 -0500

    Cleaning

--- src/Surv_Uni/Surv_Uni_One_Split_Cont.cpp
+++ src/Surv_Uni/Surv_Uni_One_Split_Cont.cpp
@@ -19,9 +19,8 @@ void Surv_Uni_Split_Cont(Uni_Split_Class& TempSplit,
                          const uvec& Censor, // Censor is collapsed
                          size_t NFail,
                          const uvec& All_Fail,
-                         const uvec& All_Risk,
+                         const vec& All_Risk,
                          vec& Temp_Vec,//Constant interferes with later calculations
-                         double penalty,
                          int split_gen,
                          int split_rule,
                          int nsplit,
@@ -35,16 +34,7 @@ void Surv_Uni_Split_Cont(Uni_Split_Class& TempSplit,
     size_t temp_ind;
     double temp_score = -1;
     size_t N = obs_id.n_elem;
-    
-    // initiate the hazard and log-likelihood for split_rule>2
-    // vec lambda0(NFail+1, fill::zeros);
-    double Loglik0 = 0;
-    // 
-    if(split_rule==3 or split_rule==4){
-    //   lambda0 = hazard(All_Fail, All_Risk);
-       Loglik0 = dot(All_Fail, log(Temp_Vec.replace(0, 1))) - dot(All_Risk, Temp_Vec);
-     }
-    
+
     //Rcout << " data here \n" << join_rows(All_Fail, All_Risk) << std::endl;
         
     uvec Left_Risk(NFail+1);
@@ -56,38 +46,33 @@ void Surv_Uni_Split_Cont(Uni_Split_Class& TempSplit,
     {
         for (int k = 0; k < nsplit; k++)
         {
-            
+
             // generate a random cut off
-            temp_cut_arma = x(obs_id( (size_t) intRand(0, N-1) ));
-            temp_cut = temp_cut_arma(0);
-            
+            temp_cut_arma = x(obs_id((size_t) intRand(0, N-1) ));
+            temp_cut = temp_cut_arma(0);//x(obs_id((size_t) temp_cut_arma2(k)));
+
             Left_Risk.zeros();
             Left_Fail.zeros();
-            
-            for (size_t i = 0; i<obs_id.n_elem; i++)
+
+            for (size_t i = 0; i<N; i++)
             {
                 if (x(obs_id(i)) <= temp_cut)
                 {
                     Left_Risk(Y(i)) ++;
-                    
+
                     if (Censor(i) == 1)
                         Left_Fail(Y(i)) ++;
                 }
             }
-            
+
             //Rcout << "Calculating Split... "  << std::endl;
-            if (split_rule == 1)
-                temp_score = logrank(Left_Fail, Left_Risk, All_Fail, All_Risk);
-                
+            if (split_rule == 1){
+              temp_score = logrank(Left_Fail, Left_Risk, All_Fail, All_Risk);
+            }
+
             if (split_rule == 2)
                 temp_score = suplogrank(Left_Fail, Left_Risk, All_Fail, All_Risk, Temp_Vec);
-            
-            if (split_rule == 3 or split_rule == 4)
-                temp_score = loglik(Left_Fail, Left_Risk, All_Fail, All_Risk, Temp_Vec, Loglik0);
-            
-            if(split_rule == 4)
-                temp_score = temp_score * penalty;
-            
+
             if (temp_score > TempSplit.score)
             {
                 TempSplit.value = temp_cut;
@@ -138,12 +123,14 @@ void Surv_Uni_Split_Cont(Uni_Split_Class& TempSplit,
     {
         DEBUG_Rcout << "      --- Surv_One_Split_Cont with " << nsplit << " rank split " << std::endl;
         
+        Vector<INTSXP> temp_ind_all = sample(highindex-lowindex, nsplit)+lowindex-1;
+        
         for (int k = 0; k < nsplit; k++)
         {
 
             // generate a cut off
-            temp_ind = intRand(lowindex, highindex);
-            
+            temp_ind = (size_t) temp_ind_all(k);
+          
             Left_Risk.zeros();
             Left_Fail.zeros();
             
@@ -160,12 +147,6 @@ void Surv_Uni_Split_Cont(Uni_Split_Class& TempSplit,
             
             if (split_rule == 2)
                 temp_score = suplogrank(Left_Fail, Left_Risk, All_Fail, All_Risk, Temp_Vec);
-            
-            if (split_rule == 3 or split_rule == 4)
-              temp_score = loglik(Left_Fail, Left_Risk, All_Fail, All_Risk, Temp_Vec, Loglik0);
-            
-            if(split_rule == 4)
-              temp_score = temp_score * penalty;
             
             if (temp_score > TempSplit.score)
             {
@@ -212,14 +193,6 @@ void Surv_Uni_Split_Cont(Uni_Split_Class& TempSplit,
             if (split_rule == 2)
                 temp_score = suplogrank(Left_Fail, Left_Risk, All_Fail, All_Risk, Temp_Vec);
             
-            if (split_rule == 3 or split_rule == 4)
-              temp_score = loglik(Left_Fail, Left_Risk, All_Fail, All_Risk, Temp_Vec, Loglik0);
-            
-            if(split_rule == 4)
-              temp_score = temp_score * penalty;
-            
-            //Rcout << temp_score << std::endl;
-            
             if (temp_score > TempSplit.score)
             {
                 TempSplit.value = (x(indices(i)) + x(indices(i+1)))/2;
@@ -240,46 +213,236 @@ void Surv_Uni_Split_Cont(Uni_Split_Class& TempSplit,
     }
 }
 
+//Function for the CoxGrad split, since it uses a psuedo outcome
+//Simpler than main function
+void Surv_Uni_Split_Cont_Pseudo(Uni_Split_Class& TempSplit, 
+                         uvec& obs_id,
+                         const vec& x,
+                         const uvec& Y, // Y is collapsed
+                         const uvec& Censor, // Censor is collapsed
+                         size_t NFail,
+                         vec& z_eta,
+                         int split_gen,
+                         int nsplit,
+                         size_t nmin, 
+                         double alpha,
+                         bool failforce)
+{
+  
+  vec temp_cut_arma;
+  double temp_cut;
+  size_t temp_ind;
+  double temp_score = -1;
+  size_t N = obs_id.n_elem;
+
+  //Rcout << " data here \n" << join_rows(All_Fail, All_Risk) << std::endl;
+  
+  uvec Pseudo_X(N);
+  Pseudo_X.zeros();
+  
+  //Rcout << "Starting random split " << std::endl;
+  
+  if (split_gen == 1) // random split
+  {
+    for (int k = 0; k < nsplit; k++)
+    {
+       
+      temp_cut_arma = x(obs_id((size_t) intRand(0, N-1) ));
+      temp_cut = temp_cut_arma(0);
+      //Rcout << "Splitting at "<<temp_cut << std::endl;
+       
+      Pseudo_X.zeros();
+       
+       //Everything in the left node gets X=1
+       for (size_t i = 0; i<N; i++)
+       {
+         if (x(obs_id(i)) <= temp_cut)
+         {
+           Pseudo_X(i) = 1;
+         }
+       }       
+    //   //Rcout << "Calculating Split... "  << std::endl;
+    //   
+       temp_score = CoxGrad(Pseudo_X, z_eta);
+     
+       if (temp_score > TempSplit.score)
+       {
+         TempSplit.value = temp_cut;
+         TempSplit.score = temp_score;
+       }
+    }
+    return;
+  }
+  
+  uvec obs_ranked = sort_index(x(obs_id)); // this is the sorted obs_id
+  uvec indices = obs_id(obs_ranked); // this is the sorted obs_id
+  
+  // check identical 
+  if ( x(indices(0)) == x(indices(N-1)) ) return;  
+  
+  // set low and high index
+  size_t lowindex = 0; // less equal goes to left
+  size_t highindex = N - 2;
+  
+  // alpha is only effective when x can be sorted
+  // this will force nmin for each child node
+  if (alpha > 0)
+  {
+    if (N*alpha > nmin) nmin = (size_t) N*alpha;
+    
+    // if there are ties, do further check
+    if ( (x(indices(lowindex)) == x(indices(lowindex + 1))) | (x(indices(highindex)) == x(indices(highindex + 1))) )
+      move_cont_index(lowindex, highindex, x, indices, nmin);
+    
+  }else{
+    // move index if ties
+    while( x(indices(lowindex)) == x(indices(lowindex + 1)) ) lowindex++;
+    while( x(indices(highindex)) == x(indices(highindex + 1)) ) highindex--;    
+    
+    if (lowindex > highindex) return;
+  }
+  
+  // force number of failures
+  
+  if (failforce)
+  {
+    Rcout << " failforce not implemented yet " << std::endl;
+  }
+  
+  
+  if (split_gen == 2) // rank split
+  {
+    DEBUG_Rcout << "      --- Surv_One_Split_Cont with " << nsplit << " rank split " << std::endl;
+    
+    for (int k = 0; k < nsplit; k++)
+    {
+      
+      // generate a cut off
+      temp_ind = (size_t) sample(N, 1)(0)-1;
+      
+      for (size_t i = 0; i <= temp_ind; i++)
+      {
+        Pseudo_X(obs_ranked(i)) = 1;
+      }
+      
+        temp_score = CoxGrad(Pseudo_X, z_eta);
+
+      if (temp_score > TempSplit.score)
+      {
+        TempSplit.value = (x(indices(temp_ind)) + x(indices(temp_ind+1)))/2 ;
+        TempSplit.score = temp_score;
+      }
+    }
+    
+    return;
+  }
+  
+  if (split_gen == 3) // best split  
+  {
+    
+    // initiate the failure and censoring counts
+    for (size_t i = 0; i<= lowindex; i++)
+    {
+      Pseudo_X(obs_ranked(i)) = 1;
+    }
+
+    for (size_t i = lowindex; i <= highindex; i++)
+    {
+      // to use this, highindex cannot be a tie location. 
+      // This should be checked already at move_cont_index
+      
+      while (x(indices(i)) == x(indices(i+1))){
+        i++;
+        
+        Pseudo_X(obs_ranked(i)) = 1;
+      }
+      
+        temp_score = CoxGrad(Pseudo_X, z_eta);
+
+      //Rcout << "Score at "<<(x(indices(i)) + x(indices(i+1)))/2 << ": "<<temp_score << std::endl;
+      
+      if (temp_score > TempSplit.score)
+      {
+        TempSplit.value = (x(indices(i)) + x(indices(i+1)))/2;
+        TempSplit.score = temp_score;
+        
+      }
+      
+      if (i + 1 <= highindex)
+      {
+        Pseudo_X(obs_ranked(i+1)) = 1;
+      }
+    }
+
+    return;
+  }
+}
+
+//Comments include old, slower logrank version
 double logrank(const uvec& Left_Fail, 
                const uvec& Left_Risk, 
                const uvec& All_Fail, 
-               const uvec& All_Risk)
+               const vec& All_Risk)
 {
     uvec Left_Risk_All(Left_Risk.n_elem);
+    Left_Risk_All.zeros();
     Left_Risk_All(0) = accu(Left_Risk);
-        
+
     if (Left_Risk_All(0) == 0 or Left_Risk_All(0) == All_Risk(0))
-        return -1; 
-        
+        return -1;
+
     for (size_t k = 1; k < Left_Risk_All.n_elem; k++)
     {
         Left_Risk_All(k) = Left_Risk_All(k-1) - Left_Risk(k-1);
     }
     
+    // uvec Left_Risk_All = shift(reverse(cumsum(Left_Risk)))
+    // Left_Risk_All(0) = accu(Left_Risk);
+    
     //Rcout << " \n left node \n" << join_rows(Left_Fail, Left_Risk_All) << std::endl;
     
-    vec SizeRatio = conv_to< vec >::from(Left_Risk_All) / All_Risk;
+    double var = 0;
+    double diff = 0;
+
+    //vec SizeRatio = conv_to< vec >::from(Left_Risk_All) / All_Risk;
+    
+    for(size_t i=0; i < Left_Risk_All.n_elem; i++){
+      if(All_Risk(i)>=2){
+        var += Left_Risk_All(i)/All_Risk(i) * (1.0-Left_Risk_All(i)/All_Risk(i)) * All_Fail(i) * 
+          (All_Risk(i) - All_Fail(i))/(All_Risk(i)-1);
+      }
+      diff += Left_Fail(i)- Left_Risk_All(i)/All_Risk(i) * All_Fail(i);
+    }
+    //Check Armadillo for difference between [] and ()
+    //Can make them the same by turning off boundary check
+    
     
     // Variance: N_{1j} / N_{j} * (1 - N_{1j} / N_{j}) * O_{j} * ( N_{j} - O_{j} ) / (N_{j} - 1)
-    vec var = SizeRatio % (1.0 - SizeRatio) % All_Fail % (All_Risk - All_Fail) / (All_Risk - 1);
+    //vec var = SizeRatio % (1.0 - SizeRatio) % All_Fail % (All_Risk - All_Fail) / (All_Risk - 1);
     
     // Difference: O_{1j} - N_{1j} * O_{j} / N_{j}
-    vec diff = Left_Fail - SizeRatio % All_Fail;
+    //vec diff = Left_Fail - SizeRatio % All_Fail;
     
-    for (size_t i = 0; i < All_Risk.n_elem; i++)
-        if (All_Risk(i) < 2)
-            var(i) = 0;
+    //uvec small = find(All_Risk < 2);
+    //A.elem( find(A > 0.5) ).ones();
+    //var.elem( small ) = zeros<vec>(small.n_elem);
+    //for (size_t i = 0; i < All_Risk.n_elem; i++)
+    //    if (All_Risk(i) < 2)
+    //        var(i) = 0;
+    
         
-    double num = accu(diff);
+    double num = diff;//accu(diff);
     
-    return num*num/accu(var);
+    //Rcout << num*num/var << std::endl;
+    
+    return num*num/var;//accu(var);
 }
 
 
 double suplogrank(const uvec& Left_Fail, 
                   const uvec& Left_Risk, 
                   const uvec& All_Fail, 
-                  const uvec& All_Risk, 
+                  const vec& All_Risk, 
                   vec& Temp_Vec)
 {
     uvec Left_Risk_All(Left_Risk.n_elem);
@@ -320,74 +483,18 @@ double suplogrank(const uvec& Left_Fail,
     return max(terms);
 }
 
-
-vec hazard(const uvec& Fail, 
-           const uvec& Risk)
+//Cox gradient split rule implementation
+double CoxGrad(uvec& Pseudo_X,
+           const vec& z_eta)
 {
-    //Rcout << "Calculating hazard..."  << std::endl;
-    vec haz = conv_to< vec >::from(Fail)/Risk;
-    
-    //datum::nan replace with 0 handles case where the at-risk
-    ///set is empty after a certain timepoint (i.e., all large values of Y are in left node)
-    return haz.replace(datum::nan, 0);
-}
-
-
-double loglik(const uvec& Left_Fail, 
-              const uvec& Left_Risk, 
-              const uvec& All_Fail, 
-              const uvec& All_Risk,
-              vec& lambda0,
-              double& Loglik0)
-{
-    // cumulative at risk count
-    uvec Left_Risk_All(Left_Risk.n_elem);
-    Left_Risk_All(0) = accu(Left_Risk);
-    
-    if (Left_Risk_All(0) == 0 or Left_Risk_All(0) == All_Risk(0))
-      return -1;
-    
-    for (size_t k = 1; k < Left_Risk_All.n_elem; k++)
-    {
-        Left_Risk_All(k) = Left_Risk_All(k-1) - Left_Risk(k-1);
-    }
-
-    vec lambdaLtmp = hazard(Left_Fail, Left_Risk_All);
-    vec lambdaRtmp = hazard(All_Fail-Left_Fail, All_Risk-Left_Risk_All);
-    
-    double epsilon = 0.1;
-    
-    //double scl = 1.0/((Left_Risk_All(0)*accu(lambdaLtmp)+(All_Risk(0)-Left_Risk_All(0))*accu(lambdaRtmp))/All_Risk(0)-accu(lambda0));
-    
-    vec lambdaL = (lambdaLtmp-lambda0)*epsilon+lambda0;//
-    vec lambdaR = (lambdaRtmp-lambda0)*epsilon+lambda0;
-    
-    //vec logLtmp  = log(lambdaLtmp-lambda0);
-    //vec logRtmp  = log(lambdaRtmp-lambda0);
-    // Rcout << "logLtmp.is_finite(): " << logLtmp.is_finite()  << std::endl;
-    // Rcout << "logLtmp(logLtmp.n_elem-1): " << logLtmp(logLtmp.n_elem-1)  << std::endl;
-    //logLtmp.elem( find_nonfinite(logLtmp) ).zeros();
-    //logRtmp.elem( find_nonfinite(logRtmp) ).zeros();
-    // //if(logLtmp.has_inf()==1) logLtmp.replace(-datum::inf, 0);
-    // Rcout << "accu(logLtmp%logLtmp): " << accu(logLtmp%logLtmp)  << std::endl;
-    // Rcout << "logLtmp(logLtmp.n_elem-1): " << logLtmp(logLtmp.n_elem-1)  << std::endl;
-    //if(logRtmp.has_inf()==1) logRtmp.replace(-datum::inf, 0);
-        
-    //vec loglambdaL = logLtmp/sqrt(accu((lambdaLtmp%lambdaLtmp)))*epsilon+log(lambda0.replace(0, 1));//
-    //vec loglambdaR = logRtmp/sqrt(accu((lambdaRtmp%lambdaRtmp)))*epsilon+log(lambda0.replace(0, 1));
-    
-    //Rcout << "lambdaL(0): " << lambdaL(0)  << std::endl;
-    // left and right log-likelihood
-    double loglikL = dot(Left_Fail, log(lambdaL)) - dot(Left_Risk_All, lambdaL);//Alternatively, dot(Left_Fail, log(lambdaL.replace(0, 1))) - dot(Left_Risk, cumsum(lambdaL))
-    double loglikR = dot(All_Fail-Left_Fail, log(lambdaR)) - dot(All_Risk-Left_Risk_All, lambdaR);
-    //double loglikL = dot(Left_Fail, loglambdaL) - dot(Left_Risk_All, exp(loglambdaL));//Alternatively, dot(Left_Fail, log(lambdaL.replace(0, 1))) - dot(Left_Risk, cumsum(lambdaL))
-    //double loglikR = dot(All_Fail-Left_Fail, loglambdaR) - dot(All_Risk-Left_Risk_All, exp(loglambdaR));
-    double loglik_ep = loglikL + loglikR;
-    //double loglik0 = dot(All_Fail, log(lambda0.replace(0, 1))) - dot(All_Risk, lambda0);
-    // Rcout << "loglikL: " << loglikL  << std::endl;
-    // Rcout << "loglikR: " << loglikR  << std::endl;
-    // Rcout << "loglik0: " << Loglik0  << std::endl;
-    // Rcout << "(loglik_ep-Loglik0)/epsilon: " << (loglik_ep-Loglik0)/epsilon  << std::endl;
-    
-    return (loglik_ep-Loglik0)/epsilon;
+  //Checking for 0 size node
+  if (accu(Pseudo_X) == 0 or accu(Pseudo_X)==Pseudo_X.n_elem)
+    return -1;
+  
+  //Calculate score
+  double imp = 0;
+  for(size_t i = 0; i<Pseudo_X.n_elem; i++){
+    imp += z_eta(i)*Pseudo_X(i);
+  }
+  return(imp*imp);
 }
